4694e789d6a748791582afdee464dd4b2b8ffd91
unknown <shaunak12wankhade@ggmail.com>
2021-10-31 00:48:18 +0530

    [Shaunak] Added : MoodAnalyser object

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSTestMoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MSTest MoodAnalyser");
+        }
+    }
+}
